e2c6d6af106224bf5443409e2d49a844fa6a3b37
metinozkn <metin.ozkn@hotmail.com>
2020-08-19 14:19:32 +0300

    added some images, used gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@metinozkan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `photos`,
+        path: `${__dirname}/src/photos`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import "./index.css"
+const IndexPage = ({ data }) => {
+  console.log("data", data)
+  return (
+    <div>
+      <h1>Merhaba</h1>
+      <div className="photos">
+        {data.allFile.edges.map(({ node }) => {
+          return <Img key={node.id} fluid={node.childImageSharp.fluid} />
+        })}
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  )
+}
 
+export const photosQuery = graphql`
+  {
+    allFile(filter: { absolutePath: { regex: "//photos//" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 100, maxHeight: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default IndexPage
